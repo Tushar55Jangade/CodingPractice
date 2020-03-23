b134757bb9b1c7dc1bf2939e6a6e520ce173e2b4
Tushar Jangade <tushar55jangade@gmail.com>
2020-03-23 01:07:58 -0700

    my practice code

---
+++ leetCodeProblems/src/consultAddCode/hi.java
@@ -0,0 +1,10 @@
+package consultAddCode;
+
+public class hi extends Thread{
+	public void run() {
+		for(int i=0;i<100;i++)
+		{
+			System.out.println("I am Writting" +i);
+		}
+	}
+}
--- leetCodeProblems/src/leetCodeProblems/intersectionOfTwoArray.java
+++
@@ -1,8 +0,0 @@
-package leetCodeProblems;
-
-public class intersectionOfTwoArray {
-	public static void main(String[] args)
-	{
-		
-	}
-}
